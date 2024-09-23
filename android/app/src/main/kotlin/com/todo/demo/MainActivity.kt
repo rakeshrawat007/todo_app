b9ae2d73767640787589e23c73461e59db9d4d22
rakesh <rakeshrawat456@gmail.com>
2024-09-23 13:18:23 +0530

    implemented clean architecture

--- android/app/src/main/kotlin/com/todo/demo/MainActivity.kt
+++ android/app/src/main/kotlin/com/todo/demo/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.todo_assesment
+package com.todo.demo
 
 import io.flutter.embedding.android.FlutterActivity
 
---
+++ lib/presentation/screens/todo_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class TodoScreen extends StatelessWidget {
+  const TodoScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
